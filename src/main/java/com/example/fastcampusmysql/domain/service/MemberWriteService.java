75430bb0a028f8115274054bf937173ad5b4168b
kimbro97 <rlagudwog@gmail.com>
2023-06-29 23:09:48 +0900

    작업

---
+++ src/main/java/com/example/fastcampusmysql/domain/service/MemberWriteService.java
@@ -0,0 +1,13 @@
+package com.example.fastcampusmysql.domain.service;
+
+public class MemberWriteService {
+    public void create() {
+        /*
+            목표 - 회원정보(이메일, 닉네임, 생년월일)을 등록한다.
+                - 닉네임은 10자를 넘길 수 없다.
+            파라미터 - memberRegisterCommand
+            val member = Member.of(memberRegisterCommand)
+            memberRepository.save(member)
+         */
+    }
+}
